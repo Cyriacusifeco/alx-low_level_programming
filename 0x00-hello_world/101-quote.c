c1a8b2cfec546e65a1d01f7ce0cd7d3769072311
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-08 23:28:57 +0000

    Create more options

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - my C entry point
+ * return - always 0 (success)
+*/
+
+int main(void)
+{
+	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	puts(a);
+
+	return (1);
+}
+
