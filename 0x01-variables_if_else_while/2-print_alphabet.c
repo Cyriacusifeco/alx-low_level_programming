6d9534f8710f81c6c24fc339e2eab736a9c90c0d
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-09 23:17:11 +0000

    print all lower case alphabets with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 97;
+		while (c <= 122)
+		{
+			putchar(c);
+			c++;
+		}
+
+			putchar(10);
+
+
+
+
+
+
+
+
+
+
+return (0);
+}
