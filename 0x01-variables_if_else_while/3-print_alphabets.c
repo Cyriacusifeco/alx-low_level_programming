3f3d5472335b123fa1fee8c9efbbf3b5a5b22348
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-10 01:57:16 +0000

    upper and lowercase letters with putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point of my program
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int j;
+
+		for (c = 97; (c <= 122); c++)
+		{
+			putchar(c);
+
+		}
+
+		for (j = 65; j <= 90; j++)
+
+		{
+			putchar(j);
+
+		}
+
+			putchar(10);
+
+return (0);
+}
