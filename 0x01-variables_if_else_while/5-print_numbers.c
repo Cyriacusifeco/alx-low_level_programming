47bb4edf770a7d7eb182f761eb76acfbe94701d7
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-10 05:04:10 +0000

    Reverse

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: 0
+ */
+int main(void)
+{
+	int c = '0';
+		while (c < '10')
+
+		{
+			putchar('c');
+			c++;
+		}
+
+			putchar(10);
+
+
+
+
+
+
+
+
+
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 122;
+		while (c <= 122 && c >= 97)
+		{
+			putchar(c);
+			c--;
+		}
+
+			putchar(10);
+
+
+
+
+
+
+
+
+
+
+return (0);
+}
