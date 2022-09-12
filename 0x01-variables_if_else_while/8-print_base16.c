f01b40e82217b9d554c23f7ee7f1ddfb5b3a726e
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-12 00:46:59 +0000

    Print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point of my program
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int j;
+
+		for (c = 0; c < 10; c++)
+		{
+			putchar(c + '0');
+
+		}
+
+		for (j = 97; j <= 102; j++)
+
+		{
+			putchar(j);
+
+		}
+
+			putchar(10);
+
+return (0);
+}
