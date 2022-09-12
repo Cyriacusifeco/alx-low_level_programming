4bca56fd354aedb4d7514a00c68b7dd5adc2a99d
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-12 00:17:47 +0000

    Print single numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	while (c < 10)
+
+
+	{
+		putchar(c + '0');
+			c++;
+	}
+
+		putchar(10);
+
+
+
+
+
+
+
+
+
+
+return (0);
+}
