5c9b3d69139ef9645cdd47d07718a48e85f18009
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-13 20:14:35 +0000

    Print all lowercase letters 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,10 @@
 #include "main.h"
 /**
- * main - Entry point of program
- * print_alphabet - Entry point for program.
+ * print_alphabet_x10 - Entry point of program
+ * print_alphabet - Entry point for program
+ *
+ * Description: This program prints lowercase letter 10 times
+ * Return: No return value is expected
  *
  */
 
@@ -20,17 +23,23 @@ void print_alphabet_x10(void)
 	print_alphabet();
 	print_alphabet();
 
-		
+
 }
+/**
+ * print_alphabet - Entry point for called function
+ *
+ * return is void
+ */
 
 void print_alphabet(void)
 
-{ 		int c;
+{	int c;
 
-	        for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 
-		{       _putchar(c);
-		}
-		       _putchar('\n');
+	{   _putchar(c);
+	}
+
+		_putchar('\n');
 
 }
