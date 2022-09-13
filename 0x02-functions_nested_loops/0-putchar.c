07aa9f96c277595a4f01046a362d2de4b5b1a443
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-13 15:29:07 +0000

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - Entry point of program
+ *
+ * Return - Always 0
+ */
+
+int _putchar(char c);
+
+int main(void)
+
+{	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+
+	_putchar('\n');
+
+
+return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+The following piece of codes perform specific tasks and can be called by its function.
