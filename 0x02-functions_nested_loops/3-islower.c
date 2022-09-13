2d84dc50cf3c120c4e36439d3cc45c6ebdf6b64e
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-13 20:38:00 +0000

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point for program
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+
+
+int _islower(int c);
+
+int _islower(int c)
+{
+	if (c >=97 || c <= 122)
+	{
+		_putchar(1 + '0');
+	else _putchar(0 + '0');
+	}
+		_putchar('\n');
+return (0);
+}
