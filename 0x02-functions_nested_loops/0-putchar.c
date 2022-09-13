712bd4fdc169c0e73b97963956bdd4c766066f99
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-13 15:43:56 +0000

    Print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,15 @@
 /**
  * main - Entry point of program
  *
- * Return - Always 0
+ * return: - Always 0
+ *
+ * _putchar - writes the character c to stdout
+ *
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ *
  */
 
 int _putchar(char c);
