adcfc73bc42f02ba03cac0bac9e1c61c488162a1
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-13 21:58:05 +0000

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,8 +11,7 @@
 
 int _islower(int c)
 {
-	int c;
-
+	
 	if (c >=97 && c <= 122)
 	{
 		_putchar (1 + '0');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,6 @@
 
 */
 
-void print_alphabet_x10(void);
+int _islower(int c);
 
 int _putchar(char );
