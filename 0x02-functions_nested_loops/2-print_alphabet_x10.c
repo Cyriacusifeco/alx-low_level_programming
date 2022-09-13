8c2b796a0d481d3edf79a27e8fef9538e066fd66
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-13 19:55:23 +0000

    10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,20 +7,20 @@
 
 void print_alphabet(void);
 
-int main(void)
+void print_alphabet_x10(void)
 {
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
 
-		print_alphabet();
-		print_alphabet();
-		print_alphabet();
-		print_alphabet();
-		print_alphabet();
-		print_alphabet();
-		print_alphabet();
-		print_alphabet();
-		print_alphabet();
-		print_alphabet();
-return(0);
+		
 }
 
 void print_alphabet(void)
