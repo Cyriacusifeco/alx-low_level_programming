58c11e24c373b4d5f74838b37d42041e71bdf32a
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-13 16:20:52 +0000

    print all lowercase characters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,8 @@
  *
  * _putchar - writes the character c to stdout
  *
- * @c: The character to print
+ *
+ * print_alphabet - prints all lower case characteret
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -28,7 +29,7 @@ void print_alphabet(void)
 
 { int c;
 
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 
 	{	_putchar(c);
 	}
