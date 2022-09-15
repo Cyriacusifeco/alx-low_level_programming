89327457ce2f869a7c97af13d12bbb38a95a7214
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-15 21:49:16 +0000

    Find the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -27,4 +27,4 @@ int largest_number(int a, int b, int c)
 	}
 
 	return (largest);
-
+}
--- 0x03-debugging/test.c
+++ 0x03-debugging/test.c
@@ -1,43 +1,30 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/* more headers goes there */
+#include "main.h"
 
 /**
- * main - Entry point for program
- * Return: 0 on success
+ *  * largest_number - returns the largest of 3 numbers
+ *   * @a: first integer
+ *    * @b: second integer
+ *     * @c: third integer
+ *      * Return: largest number
+ *       *
  */
-int positive_or_negative(int i)
+
+int largest_number(int a, int b, int c)
 {
-		
+	int largest;
 
-			srand(time(0));
-				i = rand() - RAND_MAX / 2;
-					/* your code goes there */
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-					if (i > 0)
-					{
-						printf("%d is positive\n", i);
-					}
-					else if (i < 0)
-					{
-						printf("%d is negative\n", i);
-					}
-					else
-					{printf("%d is zero\n", i);
-					}
-					return (0);
-}
+	return (largest);
 
-
-
-
-int main(void)
-{
-	        int i;
-
-		        i = 0;
-			        positive_or_negative(i);
-
-				        return (0);
-}
