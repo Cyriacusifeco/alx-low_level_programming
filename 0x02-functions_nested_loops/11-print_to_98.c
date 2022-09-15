a748815541810dc79d6ba7714c4690603d16c139
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-15 00:35:56 +0000

    count to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,25 +3,32 @@
 /**
  * print_to_98 - Entry point for program
  * @n: The character parameter
- * 
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  *
  */
 
-void print_to_98(int n)			
+void print_to_98(int n)
+{
 
-{	while ((n >= 98) && (n <! 98))
-	{ printf("%d, ", n);
+	
+	while (n > 97)&&(n < !99)
+	{
+		printf("%d, ", n);
 		n--;
 	}
-		putchar(10);
 
-	while ((n <= 98) && (n >! 98))
-	{ printf("%d, ", n);
+
+	while (n < 98)&&(n > !97)
+	{
+		printf("%d, ", n);
 		n++;
-	
 	}
-		putchar(10);
+
+	if (n == 98)
+	{
+		printf("%d\n", n)
+
+	}
 
 }
