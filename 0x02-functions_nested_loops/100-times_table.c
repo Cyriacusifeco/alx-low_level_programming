a148f2f1f52a4b3f33c77bb6d993fe730d01a88b
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-15 03:43:23 +0000

    Know your multiplication table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - Entry point for program
+ * @n: The character parameter
+ * @i: Vertical multiplier
+ * @j: Row multiplier
+ * @a: Product
+ * 
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+
+void print_times_table(int n)
+
+{	
+	int i;
+	int j;
+	int a;
+
+	for (i=0; i<=n; i++)
+
+	{	if (n > 15 || n < 0)
+
+		{       break;
+ 		 }
+		for (j=0; j<=n; j++)
+
+		{ a = (i*j);
+
+
+		printf("%d, \t", a);
+		}
+
+		j=0;
+
+		printf("\n");
+
+	}
+
+
+}
