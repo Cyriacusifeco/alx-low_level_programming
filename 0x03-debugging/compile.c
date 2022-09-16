4dc3f19571a6973807a2cfe16e38ccaa5d60c645
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-16 01:16:23 +0000

    Check remaining days

--- 0x03-debugging/compile.c
+++ 0x03-debugging/compile.c
@@ -12,12 +12,13 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if ((year % 4 == 0) && !(year % 100 == 0) || (year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
+
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
@@ -26,6 +27,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
 	else
@@ -36,15 +38,62 @@ void print_remaining_days(int month, int day, int year)
 	}
 }
 
+int convert_day(int month, int day)
+{
+	    switch (month)
+		        {
+				        case 2:
+						            day = 31 + day;
+							                break;
+									        case 3:
+									            day = 59 + day;
+										                break;
+												        case 4:
+												            day = 90 + day;
+													                break;
+															        case 5:
+															            day = 120 + day;
+																                break;
+																		        case 6:
+																		            day = 151 + day;
+																			                break;
+																					        case 7:
+																					            day = 181 + day;
+																						                break;
+																								        case 8:
+																								            day = 212 + day;
+																									                break;
+																											        case 9:
+																											            day = 243 + day;
+																												                break;
+																														        case 10:
+																														            day = 273 + day;
+																															                break;
+																																	        case 11:
+																																	            day = 304 + day;
+																																		                break;
+																																				        case 12:
+																																				            day = 334 + day;
+																																					                break;
+																																							        default:
+																																							            break;
+																																								        }
+	        return (day);
+}
+
+
+
+
+
 int main(void)
 {
 	    int month;
 	        int day;
 		    int year;
 
-		        month = 4;
-			    day = 01;
-			        year = 1997;
+		        month = 2;
+			    day = 29;
+			        year = 2000;
 
 				    printf("Date: %02d/%02d/%04d\n", month, day, year);
 
