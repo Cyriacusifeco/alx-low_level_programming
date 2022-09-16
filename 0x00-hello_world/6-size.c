64c99d833690ddb86e561be95a19a4d47770c2e5
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-16 23:43:20 +0000

    Find various sizes of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: Always 0 on succes
+ *
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int r;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of a int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(r));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+return (0);
+}
