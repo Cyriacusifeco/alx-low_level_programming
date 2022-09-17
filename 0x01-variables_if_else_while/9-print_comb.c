c531a6cba47dd8746be64342a5c2e5b175a91c49
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-17 01:13:15 +0000

    possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: 0
+ */
+int main(void)
+
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+
+	{
+		putchar(c);
+
+		if (c < 57)
+		{
+			putchar(44);
+		}
+
+		putchar(' ');
+	}
+
+		putchar(10);
+
+
+
+
+
+
+
+
+
+
+return (0);
+}
