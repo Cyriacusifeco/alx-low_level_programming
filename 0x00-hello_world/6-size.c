45972e8f5fe64d4265dc77c0fc9d0ad8f037a514
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-17 00:23:43 +0000

    Find the size of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of a int: %ld byte(s)\n", sizeof(i));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(r));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
