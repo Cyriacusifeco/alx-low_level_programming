99d8a7d92945ae40b4957ea4b4e226ea20102cfe
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-20 00:00:58 +0000

    reset to  98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * reset_to_98 - Entry point for program
+ * @n: Initialized variable to 98
+ * Return: 0 on success
+ */
+void reset_to_98(int *n)
+{
+		*n = 98;
+
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+On my continued journey to C programming: POINTERS, ARRAYS AND STRING.
+Watch my code!
