2369acf14378af5b33dc166df1bcbf0bb47f94d7
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-23 03:36:40 +0000

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcmp -> for comparing purpose
+ *@s1: param1
+ *@s2: param2
+ *Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int comp;
+
+	comp = strcmp(s1, s2);
+
+	return (comp);
+}
