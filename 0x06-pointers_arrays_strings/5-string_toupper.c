ba7e7b8eaeb7aa4e27d2c8c8f6a8d691eb7dbe41
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-23 02:12:42 +0000

    convert to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * string_toupper - Converts string to Uppercase
+ * @s: Source string
+ *
+ * Return: return s on success
+ *
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,4 +17,6 @@ char *_strncpy(char *dest, char *src, int n);
 
 void reverse_array(int *a, int n);
 
+char *string_toupper(char *);
+
 int _putchar(char c);
