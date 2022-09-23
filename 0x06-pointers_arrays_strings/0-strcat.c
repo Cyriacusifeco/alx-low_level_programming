3d1ca39e2f0080252b9178ab407debf5db2e838f
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-23 00:44:15 +0000

    Concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,16 @@
 #include "main.h"
 /**
  * _strcat - Concatenates 2 strings
+ * @dest: Destination string
+ * @src: Source string
  *
  * Return: Concatenated version of the strings
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0; int j = 0; int c;
+	int i = 0;
+	int j = 0;
+	int c;
 
 	while (dest[i] != '\0')
 	{
