e2513b7b83dba7c4d73c592d0e763fdcf8f004b6
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-23 01:04:33 +0000

    concatenate by choice

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: Concatenated version of the strings
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	return (dest);
+}
