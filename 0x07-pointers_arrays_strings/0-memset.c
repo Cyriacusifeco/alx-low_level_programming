6a4230b5454d37caec3d24ccea9c8ba53a756599
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-26 22:12:15 +0000

    Build your memory-set function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _memset - Fills memory with constant byte
+ * @n: first n bytes of memory area
+ * @b: constant bytes to be filled
+ * @s: pointer to memory area
+ * Return: Pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	for (; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+
+
+}
