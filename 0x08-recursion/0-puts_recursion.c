88c4a53cfa04e13fdd8b891508417761cf4aee01
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-28 23:56:22 +0000

    put string with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _puts_recursion - Prints string to std output
+ * @s: poniter to string to print
+ * Return: Pointer from memory area s
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+
+
+}
