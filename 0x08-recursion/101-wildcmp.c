55c6c1cfeb7a10468793b3ff96453c1f3a7f8052
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-29 01:33:27 +0000

    compare strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * wildcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: Always 1 on success
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int sqr = 0;
+
+	sqr = s1[sqr] + s2[sqr];
+
+
+return (sqr);
+}
