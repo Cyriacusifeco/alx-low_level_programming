ea710d1319bc6d4e1f8c9dff178971fedcf83ab4
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-29 00:41:59 +0000

    Find factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * factorial - Prints the factorial of an int
+ * @n: int variable
+ * Return: Always 1 on success
+ */
+int factorial(int n)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		return (1);
+	}
+		_putchar(s[i]);
+			i++;
+
+return (1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -16,3 +16,5 @@ int _putchar(char c);
 void _print_rev_recursion(char *s);
 
 int _strlen_recursion(char *s);
+
+int factorial(int n);
