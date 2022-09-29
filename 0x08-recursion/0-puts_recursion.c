e2cb65b7a0261f409795bc9ff238c6e702e36611
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-29 23:41:17 +0000

    put with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,16 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] != '\0')
+	if (s[i] == '\0')
+	{
+		_putchar(10);
+	}
+
+	else
 	{
 		_putchar(s[i]);
 
 		_puts_recursion(s + 1);
-		_putchar(10);
 
 	}
 
