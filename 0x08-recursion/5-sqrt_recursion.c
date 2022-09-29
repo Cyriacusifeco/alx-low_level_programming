cae3e135f8099d5a0c2b37123836e28edc02493f
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-29 01:05:12 +0000

    find square

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _sqrt_recursion - Square two numbers
+ * @n: Base
+ * Return: Always 1 on success
+ */
+int _sqrt_recursion(int n)
+{
+	int sqr
+
+	sqr = x * y;
+
+
+return (sqr);
+}
