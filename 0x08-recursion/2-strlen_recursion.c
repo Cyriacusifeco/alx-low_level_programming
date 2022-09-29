46d28c9fa058367ff7f6b28f2173b179e2bd632f
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-29 23:53:19 +0000

    String lenght

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,12 +10,13 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] != '\0')
+	if (s[i] == '\0')
 	{
-		return (1);
+		return (0);
 	}
-		_putchar(s[i]);
-			i++;
 
-return (1);
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 }
