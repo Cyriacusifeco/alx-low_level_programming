a029b26773e290a6ca9421bfec430c0026014901
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-29 00:55:37 +0000

    find the power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _pow_recursion - Returns the power of an int
+ * @x: Base
+ * @y: Exponent
+ * Return: Always 1 on success
+ */
+int _pow_recursion(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+
+return (1);
+}
