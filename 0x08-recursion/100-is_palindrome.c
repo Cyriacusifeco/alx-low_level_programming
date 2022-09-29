9330230ef620d9fec21b101d5f4872ea97e40085
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-29 01:18:23 +0000

    check palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * is_palindrome - Check if input is palindrome
+ * @n: Input variable
+ * Return: Always 1 on success
+ */
+int is_prime_number(int n)
+{
+	int sqr;
+
+	sqr = 2 * n;
+
+
+return (sqr);
+}
