d1ab1c83ddf7fe6775c930b20cf86e4f0bbf1993
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-30 22:58:08 +0000

    Index script

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strstr - Entry point for program
+ * @haystack: Destination
+ * @needle: Source string
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+return (0);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcpy - Entry point for program
+ * @dest: Destination
+ * @src: Source string
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+return (0);
+}
