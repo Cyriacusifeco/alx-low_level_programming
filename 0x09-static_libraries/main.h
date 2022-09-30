e92c75a21d6fcf80f9dba3ee0ceeb1107f26cd29
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-09-30 22:04:11 +0000

    Correct from double inclusion

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * 
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
-
 */
 
 int _putchar(char c);
@@ -29,3 +30,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
