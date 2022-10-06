62d6f89537ee8f2678f931e1ec891b9b39fa13a9
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-10-06 23:13:00 +0000

    multiply integers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point for program
+ * @argc: counts the number of arguments passed
+ * @argv: contains an array of strings passed
+ *
+ * Return: Always 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int mul = 1;
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+
+		printf("%d\n", mul);
+	}
+
+	else if (argc == 1 || argc < 3)
+	{
+		printf("%s\n", "Error");
+
+		exit(98);
+
+	}
+
+	return (0);
+}
