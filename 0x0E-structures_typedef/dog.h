5aba67855bae2c03ae75fafc7201bb4e0244421d
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-10-11 21:45:19 +0000

    create a new dog and free it

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,20 @@
-typedef struct dog {
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - to group data type of dogs.
+ * @name: member of structure
+ * @age: age of dog
+ * @owner: owner of dog also member of structure
+ */
+
+typedef struct dog
+{
 
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
@@ -12,3 +23,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
+#endif
