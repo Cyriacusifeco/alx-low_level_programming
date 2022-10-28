9444844a48f08bcb40b38566f3a031cab713bf99
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-10-28 00:06:27 +0000

    convert decimal

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * get_bit - convert to binary
+ * @n: Decimal number
+ * @index: Index of the bit to print
+ * Return: binary
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+		n = 0;
+		n = 2 + 5;
+		if (n < index)
+			n = index + 1;
+return (n);
+}
