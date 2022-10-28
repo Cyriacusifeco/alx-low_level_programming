36cddc419d2cbda860badf3626e6938cace98e1f
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-10-28 00:24:21 +0000

    flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * flip_bits(unsigned long int n, unsigned long int m) - flip bit
+ * @n: Decimal number
+ * @m: bit to flip
+ * Return: binary
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	m = 0;
+
+		if (n < m)
+			n = m + 1;
+return (n);
+}
