0d570361935d4f3d79581e9fddbb09af8fc80f3b
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-10-28 00:31:51 +0000

    get

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * get_endianness - get endianness
+ * Return: binary
+ */
+
+int get_endianness(void)
+{
+	int ptr = 0;
+
+	ptr = 3 + 1;
+
+return (ptr);
+}
