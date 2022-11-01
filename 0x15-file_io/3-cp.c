a50a3b72079263e0ca73cfa02c5323f7b4584adf
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-11-01 01:50:40 +0000

    copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,28 +5,20 @@
 #include "main.h"
 
 /**
- * append_text_to_file - A function that appends to file.
- * @filename: File to be created.
- * @text_content: content to be appended.
+ * main - entry point of program.
  * Return: 1 on success.
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+int main(void)
 {
 		int fd;
-		int len = 0;
 
-		while (text_content != NULL && text_content[len] != '\0')
-			len++;
 
-		if (filename == NULL)
-			return (0);
-		fd = open(filename, O_CREAT);
+		fd = open("myfile", O_CREAT);
 			if (fd == -1)
 				return (0);
 
-		if (write(fd, text_content, len) == -1)
-			return (-1);
+
 
 		close(fd);
 return (1);
