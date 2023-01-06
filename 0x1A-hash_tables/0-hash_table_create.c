25a226ccf96e7776381f166c743bf0b910a527c1
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-06 16:08:31 +0000

    Create Hash Tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,12 +15,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
-		return NULL;
+		return (NULL);
 	table->size = size;
 
 	table->array = calloc(table->size, sizeof(hash_table_t *));
 	if (table->array == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < table->size; i++)
 	{
