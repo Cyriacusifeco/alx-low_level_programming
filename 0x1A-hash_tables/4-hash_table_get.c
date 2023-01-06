187cb510e8734a8f4171af80b2909711fb7d9192
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-06 21:09:37 +0000

    Get value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * hash_table_get - A function that retrieves a value from a hash table
+ * @key: The key to be indexed.
+ * @ht: Hash table address
+ * Return: Returns the value on success.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *head;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	head = ht->array[index];
+
+	while (strcmp(head->key, key) != 0)
+	{
+		head = head->next;
+
+		if (head == NULL)
+			return (NULL);
+	}
+
+return (head->value);
+}
