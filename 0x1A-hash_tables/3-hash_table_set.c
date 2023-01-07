839decc70cedbc25a034ac24002fd543502366c0
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-07 17:07:02 +0000

    add element

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	strcpy(new_node->key, key);
 	strcpy(new_node->value, value);
 
-	index = key_index((unsigned char *)new_node->key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	if (ht->array[index] == NULL)
 	{
