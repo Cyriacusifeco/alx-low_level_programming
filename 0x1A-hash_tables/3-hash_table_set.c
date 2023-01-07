9b479b2c81f1306a9176964ffb209e0020a5264a
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-07 18:56:46 +0000

    set table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,6 +16,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (strcmp(key, "") == 0)
 		return (0);
+	if (ht == NULL)
+		return (0);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
