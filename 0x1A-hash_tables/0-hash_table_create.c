8c81f40aa950487b83bd823fa45c3e59535836a0
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-07 12:37:40 +0000

    create tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	table->size = size;
 
-	table->array = calloc(table->size, sizeof(hash_table_t *));
+	table->array = calloc(table->size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 		return (NULL);
 
