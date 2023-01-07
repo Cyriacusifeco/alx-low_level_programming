d476ba1fc475a9fe2bef1bc8bf7961f09ba581bd
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-07 18:08:03 +0000

    get value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,17 +14,24 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *head;
 
+	if (ht == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	head = ht->array[index];
+
+	if (head == NULL)
+		return (NULL);
 
 	while (strcmp(head->key, key) != 0)
 	{
 		head = head->next;
 
-		if (head == NULL)
-			return (NULL);
 	}
+
+	if (head == NULL)
+		return (NULL);
 
 return (head->value);
 }
